4339216333e526e11daa242be30cc7d037d3494a
its-atique1987 <38153876+its-atique1987@users.noreply.github.com>
2018-04-14 15:25:32 +0530

    Update BillController.java

--- src/main/java/com/example/online/store/MyBoutique/rest/controller/BillController.java
+++ src/main/java/com/example/online/store/MyBoutique/rest/controller/BillController.java
@@ -1,12 +1,17 @@
 package com.example.online.store.MyBoutique.rest.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.online.store.MyBoutique.entity.Bill;
+import com.example.online.store.MyBoutique.entity.LineItem;
 import com.example.online.store.MyBoutique.product.model.BillDetail;
+import com.example.online.store.MyBoutique.product.model.LineItemDetail;
 import com.example.online.store.MyBoutique.rest.service.BillService;
 
 @RestController
@@ -29,9 +34,25 @@ public class BillController {
 		double subTotal = bill.getSubTotal();
 		double tax = bill.getTax();
 		
-		BillDetail billDetail = new BillDetail(total, subTotal, tax);
+		List<LineItemDetail> lineItemDetails = mapLineItemToLineItemDetail(bill.getLineItems());
+		BillDetail billDetail = new BillDetail(total, subTotal, tax, lineItemDetails);
 		System.out.println(billDetail.toString());
 		return billDetail;
+	}
+
+	private List<LineItemDetail> mapLineItemToLineItemDetail(List<LineItem> items) {
+		List<LineItemDetail> lineItemDetails = new ArrayList<LineItemDetail>();
+		for (LineItem lineItem : items) {
+			LineItemDetail detail = new LineItemDetail();
+			detail.setProductName(lineItem.getProduct().getName());
+			detail.setProductRate(lineItem.getProduct().getRate());
+			detail.setQuantity(lineItem.getQuantity());
+			detail.setTotalValue(lineItem.getTotalValue());
+			
+			lineItemDetails.add(detail);
+			
+		}
+		return lineItemDetails;
 	}
 	
 }
